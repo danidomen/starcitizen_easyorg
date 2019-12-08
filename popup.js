738a13058b38f9f9f08c3d1aaccb212542697e25
Daniel Martin <dmartin@webimpacto.es>
2019-12-08 22:29:45 +0100

    start new ui overhaul
    Based on style appearance on https://robertsspaceindustries.com/star-citizen page

--- popup.js
+++ popup.js
@@ -4,50 +4,71 @@ let saveData = document.getElementById('saveData')
 let dataSaved = document.getElementById('dataSaved')
 let addOrgMembers = document.getElementById('addOrgMembers');
 let eraseMembers = document.getElementById('eraseMembers');
+let tabs = document.querySelectorAll('.tab');
 
-document.addEventListener('DOMContentLoaded', function() {
-    init();
+for (i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener('click', function() {
+    [].forEach.call(tabs, function(el) {
+      el.classList.remove('open');
+      //el.className = el.className.replace(/\bopen\b/, "");
+    });
+    this.classList.add('open');
+  });
+}
+
+/*tabs.onclick = function (element) {
+  console.log('dentro');
+  [].forEach.call(tabs, function(el) {
+    el.classList.remove('open');
+    //el.className = el.className.replace(/\bopen\b/, "");
+  });
+  element.classList.add('open');
+
+}*/
+
+document.addEventListener('DOMContentLoaded', function () {
+  init();
 });
 
 function setChildTextNode(elementId, text) {
-    document.getElementById(elementId).innerText = text;
-  }
+  document.getElementById(elementId).innerText = text;
+}
 
-  function init() {
-    setChildTextNode('saveData', chrome.i18n.getMessage("savedatabtn"));
-    setChildTextNode('orgname_label', chrome.i18n.getMessage("orgname_label"));
-    setChildTextNode('prtnickname_label', chrome.i18n.getMessage("prtnickname_label"));
-    setChildTextNode('orgactions_label', chrome.i18n.getMessage("orgactions_label"));
-    setChildTextNode('addOrgMembers', chrome.i18n.getMessage("addmembersbtn"));
-    setChildTextNode('eraseMembers', chrome.i18n.getMessage("erasemembersbtn"));
-  }
+function init() {
+  setChildTextNode('saveData', chrome.i18n.getMessage("savedatabtn"));
+  setChildTextNode('orgname_label', chrome.i18n.getMessage("orgname_label"));
+  setChildTextNode('prtnickname_label', chrome.i18n.getMessage("prtnickname_label"));
+  setChildTextNode('orgactions_label', chrome.i18n.getMessage("orgactions_label"));
+  setChildTextNode('addOrgMembers', chrome.i18n.getMessage("addmembersbtn"));
+  setChildTextNode('eraseMembers', chrome.i18n.getMessage("erasemembersbtn"));
+}
 
-  chrome.storage.sync.get('starcitizenData', function(data) {
-    orgName.setAttribute('value', data.starcitizenData.orgName);
-    protectedNicknames.setAttribute('value', data.starcitizenData.protectedNicknames);
+chrome.storage.sync.get('starcitizenData', function (data) {
+  orgName.setAttribute('value', data.starcitizenData.orgName);
+  protectedNicknames.setAttribute('value', data.starcitizenData.protectedNicknames);
+});
+
+saveData.onclick = function (element) {
+  chrome.storage.sync.set({ starcitizenData: { "orgName": orgName.value, "protectedNicknames": protectedNicknames.value } }, function () {
+    dataSaved.classList.remove('hidden');
+    setTimeout(function () {
+      dataSaved.classList.add('hidden');
+    }, 1400)
   });
+};
 
-  saveData.onclick = function(element) {
-    chrome.storage.sync.set({starcitizenData:{"orgName": orgName.value, "protectedNicknames":protectedNicknames.value}}, function() {
-        dataSaved.classList.remove('hidden');
-        setTimeout(function(){
-            dataSaved.classList.add('hidden');
-        },1400)
+addOrgMembers.onclick = function (element) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { data: { action: "executeAddMembers", "orgName": orgName.value } }, function (response) {
+
     });
-  };
+  });
+}
 
-  addOrgMembers.onclick = function(element) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {data:{action: "executeAddMembers", "orgName": orgName.value}}, function(response) {
+eraseMembers.onclick = function (element) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { data: { action: "executeEraseMembers", "orgName": orgName.value, "protectedNicknames": protectedNicknames.value } }, function (response) {
 
-        });
     });
-  }
-
-  eraseMembers.onclick = function(element) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {data:{action: "executeEraseMembers", "orgName": orgName.value, "protectedNicknames":protectedNicknames.value}}, function(response) {
-
-        });
-    });
-  }
+  });
+}
